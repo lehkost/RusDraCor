028ce9c45e9ca308ab4720ccf68aedf7c06907f5
IrinaPavlova <ispavlovais@gmail.com>
2017-04-12 20:30:57 +0300

    parsing type-2

--- Wikisource/wikisource_to_tei_type2.py
+++ Wikisource/wikisource_to_tei_type2.py
@@ -1,1 +1,14 @@
-__author__ = 'IrinaPavlova'
+import re
+import os
+import transliterate
+
+o = open('./wikisource_raws/2/Sgovor Kutejkina.txt', 'r', encoding='utf-8')
+text_text = o.read()
+o.close()
+
+o = open('./wikisource_raws/2/Sgovor Kutejkina.txt', 'r', encoding='utf-8')
+text_lines = o
+o.close()
+
+
+def get_metadata(text):
