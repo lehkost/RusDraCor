31881cc857c88ed87e418e1abbfd927dcb1eeccf
IrinaPavlova <ispavlovais@gmail.com>
2017-04-27 18:30:54 +0300

    calculating

--- Calculating_stuff_in_plays/number_of_characters.py
+++ Calculating_stuff_in_plays/number_of_characters.py
@@ -1,1 +1,14 @@
-__author__ = 'IrinaPavlova'
+import os
+import re
+
+table = open('num_of_characters_in_plays.csv', 'w', encoding='utf-8')
+
+for file in os.listdir('./ready_TEI/'):
+    if file.endswith('.xml'):
+        table.write(file.split('.xml')[0] + ';')
+        tei = open('./ready_TEI/' + file).read()
+        characters = re.findall('<sp who="(.*?)">', tei)
+        characters = set(characters)
+        print(characters)
+        table.write(str(len(characters)) + '\n')
+table.close()
