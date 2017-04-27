2d35e14ee13745444ec86711bdce65919eb41c98
IrinaPavlova <ispavlovais@gmail.com>
2017-04-27 18:42:22 +0300

    calculating

--- Calculating_stuff_in_plays/number_of_characters.py
+++ Calculating_stuff_in_plays/number_of_characters.py
@@ -5,10 +5,9 @@ table = open('num_of_characters_in_plays.csv', 'w', encoding='utf-8')
 
 for file in os.listdir('./ready_TEI/'):
     if file.endswith('.xml'):
-        table.write(file.split('.xml')[0] + ';')
+        table.write(file.split('.xml')[0] + ',')
         tei = open('./ready_TEI/' + file).read()
         characters = re.findall('<sp who="(.*?)">', tei)
         characters = set(characters)
-        print(characters)
         table.write(str(len(characters)) + '\n')
 table.close()
